60bca993bfdcf74f954ab6ed4ae467959f6f7057
Julianna Mello <callmejuh@gmail.com>
2017-06-12 12:07:21 +0000

    Criação de controller e view para Home

---
+++ application/controllers/Home.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+	public $public;
+	
+	public function __construct(){
+		parent::__construct();
+		$this->public = base_url().'public/';
+	}
+	
+	public function index()
+	{
+		$dados = array();
+		$dados['pagina'] = 'home';
+		
+		$this->load->view('base', $dados);
+	}
+}
---
+++ application/views/home.php
@@ -0,0 +1,6 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+<div class="carousel carousel-slider">
+    <a class="carousel-item" href="#one!"><img src="<?= $this->public; ?>img/banner1.png"></a>
+    <a class="carousel-item" href="#two!"><img src="<?= $this->public; ?>img/banner2.png"></a>
+  
+</div>
