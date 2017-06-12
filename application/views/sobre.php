cd0461bbda6110f2c142b4571e22f3ee57efbb2c
Marianna Espindola <bymarimari@hotmail.com>
2017-06-12 06:50:28 +0000

    Criação controller e view da página Sobre

---
+++ application/views/sobre.php
@@ -0,0 +1,68 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+            <div class="container">
+            <h3>Sobre</h3>
+
+                <div class="sobre row">
+                    <p>Bem vindo ao movie.in, aqui você encontrará tudo que precisa saber sobre o filme que está procurando!</p> 
+                    <p>Em nosso banco de filmes você tem acesso ao poster, a sinopse e até sua nota no IMDB, informação importante se 
+                       você quer saber se o filme está fazendo sucesso e se vale a pena ser conferido, caso esteja em dúvida ainda.</p>
+                    <p>Não é necessario criar login para ter acesso ao nosso banco de filmes, mas criar um login além de ser rápido, irá aumentar
+                       sua experiência no site, e te dará acesso a nossa comunidade, contato com pessoas que curtem os mesmos filmes que você, e 
+                       ainda será possivel avaliar os seus filmes preferidos.</p>
+                </div>
+                  
+                <div class="sobre row">           
+                 <h4>Colaboradores</h4>
+                        <p> Somos 3 estudantes que curtem muito tecnologia e filmes, com o intuito de unir essas duas paixões criamos
+                            esse site pra nós, mas principalmente para você que também gosta de filmes e que ás vezes, tem dificuldade de achar
+                            informações sobre aquele filme que você está interessado mas não sabe muito sobre.</p>
+                            
+                    <div class="sobre row">
+                    <div class="sobre-imagem col s12 m3">
+                        <img src="<?= $this->public; ?>img/perfilJulianna.jpg" class="circle responsive-img">
+                    </div>
+                    <div class="sobre-texto col s12 m9">
+                        <h5>Julianna</h5>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris cursus vel orci et semper. Curabitur lobortis posuere turpis, ut varius turpis vulputate et. Vivamus eu placerat ipsum. Phasellus laoreet turpis ut imperdiet dignissim. Aenean facilisis neque neque, varius luctus orci volutpat a. Cras luctus lobortis urna vel pulvinar. Suspendisse in sapien enim. Pellentesque justo velit, pellentesque finibus tincidunt eu, efficitur in mi.</p>
+                    </div>
+                        <div class="sociais responsive-img">
+                            <a href="https://twitter.com/callmejuh"><img src="<?= $this->public; ?>img/icontwitter.png"></a>
+                            <a href="https://www.instagram.com/juhxmello"> <img src="<?= $this->public; ?>img/iconinstagram.png">
+                            <a href="https://www.facebook.com/callmejuh"><img src="<?= $this->public; ?>img/iconfacebook.png"></a>
+                        </div>
+                        
+                        
+                </div>
+                    <div class="sobre row">
+                    <div class="sobre-imagem col s12 m3">
+                        <img src="<?= $this->public; ?>img/perfilLucas.jpg"class="circle responsive-img">
+                    </div>
+                    <div class="sobre-texto col s12 m9">
+                        <h5>Lucas</h5>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris cursus vel orci et semper. Curabitur lobortis posuere turpis, ut varius turpis vulputate et. Vivamus eu placerat ipsum. Phasellus laoreet turpis ut imperdiet dignissim. Aenean facilisis neque neque, varius luctus orci volutpat a. Cras luctus lobortis urna vel pulvinar. Suspendisse in sapien enim. Pellentesque justo velit, pellentesque finibus tincidunt eu, efficitur in mi.</p>
+                    </div>
+                        <div class="sociais">
+                            <a href="https://twitter.com/biglidio"><img src="<?= $this->public; ?>img/icontwitter.png"></a>
+                            <a href="https://www.instagram.com/biglidio"> <img src="<?= $this->public; ?>img/iconinstagram.png">
+                            <a href="https://www.facebook.com/biglidio"><img src="<?= $this->public; ?>img/iconfacebook.png"></a>
+                        </div>
+                    </div>
+
+                <div class="sobre row">
+                    <div class="sobre-imagem col s12 m3">
+                        <img src="<?= $this->public; ?>img/perfilMarianna.jpg"class="circle responsive-img">
+                    </div>
+                    <div class="sobre-texto col s12 m9">
+                        <h5>Marianna</h5>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris cursus vel orci et semper. Curabitur lobortis posuere turpis, ut varius turpis vulputate et. Vivamus eu placerat ipsum. Phasellus laoreet turpis ut imperdiet dignissim. Aenean facilisis neque neque, varius luctus orci volutpat a. Cras luctus lobortis urna vel pulvinar. Suspendisse in sapien enim. Pellentesque justo velit, pellentesque finibus tincidunt eu, efficitur in mi.</p>
+                    </div>
+                        <div class="sociais">
+                                <a href="https://twitter.com/bymarimari"><img src="<?= $this->public; ?>img/icontwitter.png"></a>
+                                <a href="https://www.instagram.com/bymarimari"> <img src="<?= $this->public; ?>img/iconinstagram.png">
+                                <a href="https://pt-br.facebook.com/bymarimari"><img src="<?= $this->public; ?>img/iconfacebook.png"></a>
+                        </div>
+                </div>
+                 </div>
+               
+            </div>
