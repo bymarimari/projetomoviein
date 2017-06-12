126bc95df08d78a099ef7cac8b7309e90054a6a3
Julianna Mello <callmejuh@gmail.com>
2017-06-12 12:12:33 +0000

    Atualização do UsuarioDAO

--- application/models/UsuarioDAO.php
+++ application/models/UsuarioDAO.php
@@ -16,3 +16,24 @@ class UsuarioDAO extends CI_Model{
         $this->campos['email'] = !empty($dados['email']) ? $dados['email'] : $this->campos['email'];
         $this->campos['senha'] = !empty($dados['senha']) ? $dados['senha'] : $this->campos['senha'];
     }
+     public function find($id){
+        $this->campos = (array) $this->db->get_where($this->tabela, array($this->id_tabela => $id))->row();
+    }
+
+    public function get(){
+        return $this->campos;
+    }
+
+    public function listar(){
+        return $this->db->get_where($this->tabela)->result();
+    }
+
+    public function salvar(){
+        $this->db->set(array('nome' => $this->campos['nome'], 'email' => $this->campos['email'], 'senha' => $this->campos['senha']));
+        return empty($this->campos[$this->id_tabela]) ? $this->db->insert($this->tabela) : $this->db->update($this->tabela);
+    }
+
+    public function deletar(){
+        return $this->db->delete($this->tabela, array($this->id_tabela => $this->campos[$this->id_tabela]));
+    }
+}
